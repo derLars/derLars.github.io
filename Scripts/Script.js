da7be218962bf3bcb05a7c9e294ae03a4d83f75b
derlars <l.schwensen@hotmail.de>
2015-11-19 20:57:43 +0100

    Finished up to the current status.

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,66 +1,91 @@
 $(document).ready(function() {
     	
-    	$( "#digitalSignal" ).on( "click", function() {
-    		if(($("#digitalSignal").attr('unfolded')) == "true") {
-    			$("#digitalSignalContent").css("visibility","hidden");
-    			$( "#digitalSignal" ).animate({height:"40"},400);
-    			$("#digitalSignal").attr('unfolded', 'false');
-    		} else {
-   				$("#digitalSignalContent").css("visibility","visible");
-    			$( "#digitalSignal" ).animate({height:"350"},400);
-    			$("#digitalSignal").attr('unfolded', 'true');
-    		}    		
-    	});
+    	$( "#digitalSignalContent" ).slideUp("fast", resizeWindows());
+        $( "#powerSupplyContent" ).slideUp("fast", resizeWindows());
+        $( "#signalEncoderContent" ).slideUp("fast", resizeWindows());
+        $( "#firstTestcycleContent" ).slideUp("fast", resizeWindows());
+        $( "#theTrackContent" ).slideUp("fast", resizeWindows());
+        $( "#mainControlBoardContent" ).slideUp("fast", resizeWindows());
+        $( "#digitalSignalContent" ).slideUp("fast", resizeWindows());
+        $( "#levelShifterContent" ).slideUp("fast", resizeWindows());
+        $( "#switchControlBoardContent" ).slideUp("fast", resizeWindows());
+        $( "#secondTestcycleContent" ).slideUp("fast", resizeWindows());
+        $( "#imageProcessingContent" ).slideUp("fast", resizeWindows());
+        $( "#commingSoonContent" ).slideUp("fast", resizeWindows());
+        
+        function resizeWindows() {
+            $("#digitalSignal").css("width",$("#introduction").width());
+            $("#powerSupply").css("width",$("#introduction").width());
+            $("#signalEncoder").css("width",$("#introduction").width());
+            $("#firstTestcycle").css("width",$("#introduction").width());
+            $("#theTrack").css("width",$("#introduction").width());
+            $("#mainControlBoard").css("width",$("#introduction").width());
+            $("#levelShifter").css("width",$("#introduction").width());
+            $("#switchControlBoard").css("width",$("#introduction").width());
+            $("#secondTestcycle").css("width",$("#introduction").width());
+            $("#imageProcessing").css("width",$("#introduction").width());
+            $("#commingSoon").css("width",$("#introduction").width());
+        }
 
-    	
-    	$( "#powerSupply" ).on( "click", function() {
-    		if(($("#powerSupply").attr('unfolded')) == "true") {
-    			$("#powerSupplyContent").css("visibility","hidden");
-    			$( "#powerSupply" ).animate({height:"40"},400);
-    			$("#powerSupply").attr('unfolded', 'false');
-    		} else {
-   				$("#powerSupplyContent").css("visibility","visible");
-    			$( "#powerSupply" ).animate({height:"1500"},400);	
-    			$("#powerSupply").attr('unfolded', 'true');
-    		}    		
-    	});
+        $( "#digitalSignalButton" ).on( "click", function() {           
+            $( "#digitalSignalContent" ).slideToggle( "slow", resizeWindows())
+        });
 
-    	$( "#signalEncoder" ).on( "click", function() {
-    		if(($("#signalEncoder").attr('unfolded')) == "true") {
-    			$("#signalEncoderContent").css("visibility","hidden");
-    			$( "#signalEncoder" ).animate({height:"40"},400);
-    			$("#signalEncoder").attr('unfolded', 'false');
-    		} else {
-   				$("#signalEncoderContent").css("visibility","visible");
-    			$( "#signalEncoder" ).animate({height:"1100"},400);			
-    			$("#signalEncoder").attr('unfolded', 'true');
-    		}    		
-    	});
+        $( "#powerSupply" ).on( "click", function() {        
+            $( "#powerSupplyContent" ).slideToggle( "slow", resizeWindows())                  
+        });
 
-    	$( "#firstTestcycle" ).on( "click", function() {
-    		if(($("#firstTestcycle").attr('unfolded')) == "true") {
-    			$("#firstTestcycleContent").css("visibility","hidden");
-    			$( "#firstTestcycle" ).animate({height:"40"},400);
-    			$("#firstTestcycle").attr('unfolded', 'false');
-    		} else {
-   				$("#firstTestcycleContent").css("visibility","visible");
-    			$( "#firstTestcycle" ).animate({height:"400"},400);
-    			$("#firstTestcycle").attr('unfolded', 'true');
-    		}    		
-    	});
+        $( "#signalEncoderButton" ).on( "click", function() {        
+            $( "#signalEncoderContent" ).slideToggle( "slow", resizeWindows())         
+        });
+        
+        $( "#firstTestcycleButton" ).on( "click", function() {       
+            $( "#firstTestcycleContent" ).slideToggle( "slow", resizeWindows())
+        });
 
-    	$( "#unfoldAll" ).on( "click", function() {
-				$("#firstTestcycleContent").css("visibility","visible");
-    			$( "#firstTestcycle" ).animate({height:"400"},400);		
-    			$("#firstTestcycle").attr('unfolded', 'true');
-    			$("#signalEncoderContent").css("visibility","visible");
-    			$( "#signalEncoder" ).animate({height:"1100"},400);			
-    			$("#signalEncoder").attr('unfolded', 'true');
-    			$("#powerSupplyContent").css("visibility","visible");
-    			$( "#powerSupply" ).animate({height:"1500"},400);	
-    			$("#powerSupply").attr('unfolded', 'true');
-   				$("#digitalSignalContent").css("visibility","visible");
-    			$( "#digitalSignal" ).animate({height:"350"},400);
-    			$("#digitalSignal").attr('unfolded', 'true');
-		});
+        $( "#theTrackButton" ).on( "click", function() {       
+            $( "#theTrackContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $( "#mainControlBoardButton" ).on( "click", function() {       
+            $( "#mainControlBoardContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $( "#levelShifterButton" ).on( "click", function() {       
+            $( "#levelShifterContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $( "#switchControlBoardButton" ).on( "click", function() {       
+            $( "#switchControlBoardContent" ).slideToggle( "slow", resizeWindows())
+        });
+  	
+        $( "#secondTestcycleButton" ).on( "click", function() {       
+            $( "#secondTestcycleContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $( "#imageProcessingButton" ).on( "click", function() {       
+            $( "#imageProcessingContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $( "#commingSoonButton" ).on( "click", function() {       
+            $( "#commingSoonContent" ).slideToggle( "slow", resizeWindows())
+        });
+
+        $(window).resize(function() {
+            resizeWindows();
+        });
+
+        $( "#unfoldAll" ).on( "click", function() {
+            $( "#digitalSignalContent" ).slideDown("fast", resizeWindows());
+            $( "#powerSupplyContent" ).slideDown("fast", resizeWindows());
+            $( "#signalEncoderContent" ).slideDown("fast", resizeWindows());
+            $( "#firstTestcycleContent" ).slideDown("fast", resizeWindows());
+            $( "#theTrackContent" ).slideDown("fast", resizeWindows());
+            $( "#mainControlBoardContent" ).slideDown("fast", resizeWindows());
+            $( "#levelShifterContent" ).slideDown("fast", resizeWindows());
+            $( "#switchControlBoardContent" ).slideDown("fast", resizeWindows());
+            $( "#secondTestcycleContent" ).slideDown("fast", resizeWindows());
+            $( "#imageProcessingContent" ).slideDown("fast", resizeWindows());
+             $( "#commingSoonContent" ).slideDown("fast", resizeWindows());
+        });
 });
